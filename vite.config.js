9916dd60cdd4bc41e9a26ba001f22008d1c63021
Yuki Ueno <y0uk10126@gmail.com>
2023-03-12 19:43:01 +0900

    Github Actionsのworkflowを修正する

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ import vuetify from 'vite-plugin-vuetify'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:'/Online-Dating-App-Vue/',
   plugins: [
 		vue(),
 		vuetify({ autoImport: true }),
